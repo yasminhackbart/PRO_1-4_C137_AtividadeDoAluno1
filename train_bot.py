3235ed76e8883b47ebdb5742854419a1d8487b7d
Pedro Eduardo Bibiano Pereira <80981386+pedroeduardobibiano@users.noreply.github.com>
2023-04-25 15:22:33 -0300

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,14 @@
+#Biblioteca de pré-processamento de dados de texto
+
+
+#função para anexar palavras-tronco
+
+
+
+    
+        # Adicione todas as palavras dos padrões à lista
+       
+        # Adicione todas as tags à lista classes
+
+
+#Crie o corpus de palavras para o chatbot
